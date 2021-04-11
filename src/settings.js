2edec577265b7eb246cd97aa4ff50f2dcb808ada
Aleš Trunda <alestrunda@gmail.com>
2021-04-11 20:21:41 +0200

    set aws api

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://vocabulary-scholar-server.azurewebsites.net'; //http://localhost:8080 if run locally
+export const API_URL = 'https://a1wh113qa6.execute-api.us-east-1.amazonaws.com'; //http://localhost:8080 if run locally
 
 export const GOOGLE_API_KEY = 'AIzaSyDk8tDeUmu-ONpWDKJnD-h4-npqOtg4udw';
 export const GOOGLE_CLIENT_ID =
